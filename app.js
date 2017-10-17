8767ac77d5941600722be549f844a5881cb1dc87
Adrien <adrientalal@gmail.com>
2017-10-17 12:20:58 +0200

    My Portfolio, First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+        $(this).children('li').toggle()
+
+    });
+});
+
+$('.grid').masonry({
+  // set itemSelector so .grid-sizer is not used in layout
+  itemSelector: '.grid-item',
+  // use element for option
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
